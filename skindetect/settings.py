3a96705063178e74e8a20f6eaa07aeca76af4957
Cao Ha <122292320+chicaoha@users.noreply.github.com>
2024-01-24 20:51:50 +0700

    update model database, folder image

--- skindetect/settings.py
+++ skindetect/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-(-1^&turd3lnmuz64=i($3$by#(0avtwy2h69a&96t_n$%l7#)
 DEBUG = True
 
 # In settings.py
-ALLOWED_HOSTS = ['yourdomain.com', 'a226-115-75-42-123.ngrok-free.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['yourdomain.com', '5d59-103-17-88-39.ngrok-free.app', 'localhost', '127.0.0.1']
 
 
 # Application definitionpy
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     
 ]
 CORS_ALLOWED_ORIGINS = [
-    "https://a226-115-75-42-123.ngrok-free.app",  # Add your NGROK URL here
+    "https://5d59-103-17-88-39.ngrok-free.app",  # Add your NGROK URL here
     # Add other allowed origins if needed
 ]
 ROOT_URLCONF = 'skindetect.urls'
@@ -131,7 +131,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
+
 
 USE_I18N = True
 
